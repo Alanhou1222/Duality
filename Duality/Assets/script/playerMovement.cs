b69ee606357a125b1b0560594c97e1df6fc3fdda
alanhou <alanhou@umich.edu>
2022-01-29 02:30:05 -0500

    finish player movement

--- Duality/Assets/script/playerMovement.cs
+++ Duality/Assets/script/playerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float playMoveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * playMoveSpeed * Time.fixedDeltaTime);
     }
 }
