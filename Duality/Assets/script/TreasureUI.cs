44d2d26394be92b1bbaaba2bef2b9b59d81b5b48
chi-wei-lien <lien2@purdue.edu>
2022-01-30 00:35:18 -0500

    auto generate powerup, remove img from prefab

--- Duality/Assets/script/TreasureUI.cs
+++ Duality/Assets/script/TreasureUI.cs
@@ -9,12 +9,15 @@ public class TreasureUI : MonoBehaviour
     public GameObject pressEGuide;
     public float minDistance = 3f;
     public GameObject BoxUI;
+    public string powerup;
     // Update is called once per frame
     void Update() {
         if ((player.position - box.position).magnitude <= minDistance) {
             pressEGuide.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E)) {
                 Open();
+                Powerup();
+                Debug.Log(powerup);
             }
         } else {
           pressEGuide.SetActive(false);
@@ -24,5 +27,19 @@ public class TreasureUI : MonoBehaviour
     void Open() {
         BoxUI.SetActive(true);
         Time.timeScale = 0f;
+    }
+
+    void Powerup() {
+        switch(Random.Range(0, 2)) {
+          case 0:
+              powerup = "shield";
+              break;
+          case 1:
+              powerup = "speed";
+              break;
+          case 2:
+              powerup = "strength";
+              break;
+        }
     }
 }
