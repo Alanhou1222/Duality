c790da922094e48924bb0a4149eaa67a80446f59
exitialium <henrywu0424@gmail.com>
2022-01-29 19:00:01 -0500

    Character Color/ Arrow/ Laser

--- Duality/Assets/script/shooting.cs
+++ Duality/Assets/script/shooting.cs
@@ -11,11 +11,23 @@ public class shooting : MonoBehaviour
     private float nextFire;
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public playerControl controller;
+  
 
     public float bulletForce = 20f;
     // Update is called once per frame
+    void Start() 
+    {
+        controller = GameObject.Find("Player").GetComponent(typeof(playerControl)) as playerControl;    
+    }
     void Update()
     {
+        if(controller.era == playerControl.playerType.medieval) {
+            bulletForce = 12f;
+        }
+        else {
+            bulletForce = 30f;
+        }
         if(Input.GetButtonDown("Fire1") && Time.time > nextFire){
             Shoot();
             nextFire = Time.time + fireRate;
@@ -23,7 +35,15 @@ public class shooting : MonoBehaviour
     }
     
     void Shoot(){
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bullet;
+        if(controller.era == playerControl.playerType.medieval) {
+            firePoint.localPosition = new Vector3(0.05f,0.15f,0);
+            bullet = Instantiate(bulletPrefab, firePoint.position, transform.rotation);
+        }
+        else {
+            firePoint.localPosition = new Vector3(0.05f,0.2f,0);
+            bullet = Instantiate(bulletPrefab, firePoint.position, transform.rotation);
+        }
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up*bulletForce,ForceMode2D.Impulse);
     }
